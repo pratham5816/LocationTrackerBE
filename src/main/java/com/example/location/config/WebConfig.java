10c8cdc0737dd574bb204cd2539917fafd96bdc8
pratham5816 <163821803+pratham5816@users.noreply.github.com>
2025-12-10 18:12:36 +0530

    Update WebConfig.java

--- src/main/java/com/example/location/config/WebConfig.java
+++ src/main/java/com/example/location/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://location-tracker-fe.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
